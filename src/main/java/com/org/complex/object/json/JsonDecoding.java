5396ee8ec5d4c6d9ce01ac9d29ace4db452c4422
chnadra <chandrababu053@gmail.com>
2020-06-11 16:00:00 +0530

    json edited

--- src/main/java/com/org/complex/object/json/JsonDecoding.java
+++ src/main/java/com/org/complex/object/json/JsonDecoding.java
@@ -5,7 +5,7 @@ import org.json.simple.JSONValue;
 
 public class JsonDecoding {
     public static void main(String[] args) {
-        String json = "{\"chandra\":\"29\",\"naidu\":\"30\"}";
+        String json = "{\"chandra\":\"29\",\"naidu\":\"40\"}";
         Object obj = JSONValue.parse(json);
         JSONObject jsonObject = (JSONObject) obj;
         System.out.println(jsonObject.get("chandra")+" "+jsonObject.get("naidu"));
